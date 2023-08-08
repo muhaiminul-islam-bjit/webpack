c2b881e7069059ebf39c6e83787984b434e48cad
muhaiminul-islam-bjit <muhaiminul.islam@bjitgroup.com>
2023-08-08 17:23:09 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Image from "./react.svg";
+
+const App = () => {
+  return (
+    <div>
+      <h1>react Typescript Webpack</h1>
+      <img src={Image} />
+    </div>
+  );
+};
+
+export default App;
